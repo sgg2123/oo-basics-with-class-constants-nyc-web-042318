497a21afb1cf3affad55650003999a609e643a89
Sydney Garay <sgg2123@columbia.edu>
2018-04-26 02:06:55 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,19 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    result = BRANDS.any? do |existing_brand|
+      brand.downcase == existing_brand.downcase
+    end
+    if result == false
+      BRANDS << brand
+    end
   end
 
   def cobble
